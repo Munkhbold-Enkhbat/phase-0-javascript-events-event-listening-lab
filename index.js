7190f4b9219c0a905c8163a122bd8e0dadc86dae
Enkhbat Munkhbold <emunkhbold10@gmail.com>
2022-01-27 14:53:41 -0800

    commit tree d9e7d41cbd39dc49e52091db723a2086cd0de496

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
 function addingEventListener() {
+  const input = document.getElementById('input')
+  const alertMessage = () => alert('It actually works, awesome!')
+  input.addEventListener('click', alertMessage)
 }
